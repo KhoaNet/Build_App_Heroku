f26399b56648fccfa9bc46a09884bd96401434bf
ndkhoa1 <ndkhoa1@cmc.com.vn>
2021-05-17 15:34:57 +0700

    demo

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 //dev.js - don't commit this
 module.exports = {
-    //googleClientID:"525179804393-gr9d9g233ag1977naocclsn0m2j4kpft.apps.googleusercontent.com",
-	googleClientID:"184439430697-n1tr2sqqofdmomhalir4eqa2thcnndfj.apps.googleusercontent.com",  
+    googleClientID:"525179804393-gr9d9g233ag1977naocclsn0m2j4kpft.apps.googleusercontent.com",
+	//googleClientID:"184439430697-n1tr2sqqofdmomhalir4eqa2thcnndfj.apps.googleusercontent.com",  
     googleClientSecret: "MPEpDkTIgKPgzYmUUt1J2sFS",
     //mongoURI: "mongodb://CSC430-1:Dy1y5YpvyyVXOVm1Quw5@ds163918.mlab.com:63918/udemy-test" ,
     mongoURI: "mongodb+srv://user:21012014@cluster0.xp2br.mongodb.net/user?retryWrites=true&w=majority" ,
